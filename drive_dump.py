d3e21fca749e118a00537eb6daff54eb103bf508
audax <kadenbach@qabel.de>
2019-06-03 13:11:03 +0200

    Faster with MySQL 8+

--- drive_dump.py
+++ drive_dump.py
@@ -1,5 +1,4 @@
 import argparse
-import itertools
 
 import mysql.connector
 from anytree import Node, RenderTree, render
@@ -21,31 +20,68 @@ def parse_args():
     return parser.parse_args()
 
 
-def query(db, parent):
+def query_folders(db):
     cur = db.cursor()
-    cur.execute('SELECT fuid, parent, fname FROM oxfolder_tree WHERE parent=%s', (parent,))
+    cur.execute('SELECT fuid, parent, fname FROM oxfolder_tree ORDER BY parent')
     yield from cur
     cur.close()
 
 
-def build_tree(db):
-    def find_children(parent):
-        return [Node(id=id, name=fname, parent_id=parent_id)
-                for id, parent_id, fname in query(db, parent)]
+def query_files(db):
+    cur = db.cursor()
+    cur.execute("""
+SELECT t.filename,
+       t.file_store_location,
+       folder_id
+ FROM (
+                  SELECT d.filename,
+                         d.file_store_location,
+                         d.version_number,
+                         d.infostore_id,
+                         ROW_NUMBER() over (
+                             PARTITION BY infostore_id ORDER BY version_number
+                             ) AS row_num
+                  FROM infostore_document AS d
+                  WHERE d.file_store_location IS NOT NULL
+              ) AS t
+JOIN infostore ON t.infostore_id = infostore.id
+WHERE t.row_num = 1
 
-    def recur(node):
-        node.children = find_children(node.id)
-        for child in node.children:
-            recur(child)
+    """)
+    yield from cur
+    cur.close()
 
-    root = Node(name='', id=0)
-    recur(root)
+
+def build_file_nodes(rows):
+    return (Node(name=filename,
+                 location=file_store_location,
+                 folder_id=folder_id)
+            for filename, file_store_location, folder_id in rows)
+
+
+def build_nodes(rows):
+    return (Node(id=id, name=fname, parent_id=parent_id)
+                for id, parent_id, fname in rows)
+
+
+def build_tree(folder_rows, file_rows):
+    root = Node(name='', id=0, parent_id=-1)
+    node_by_id = {node.id: node for node in build_nodes(folder_rows)}
+    node_by_id[0] = root
+    files_itr = build_file_nodes(file_rows)
+    for file in files_itr:
+        folder_node = node_by_id[file.folder_id]
+        file.parent = folder_node
+    for node in node_by_id.values():
+        if node.parent_id is not None:
+            node.parent = node_by_id.get(node.parent_id)
     return root
+
 
 def main():
     args = parse_args()
     db = connect(args.host, args.user, args.password, args.port, args.db)
-    root = build_tree(db)
+    root = build_tree(list(query_folders(db)), query_files(db))
     print(RenderTree(root, style=render.ContStyle))
 
 
