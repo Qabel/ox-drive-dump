3d438f8374154e0b3eb0d884e32f63625fa7b328
audax <kadenbach@qabel.de>
2019-06-04 13:51:00 +0200

    Add option to use hardlinks instead of copy
    Progressbars are also included.

--- drive_dump.py
+++ drive_dump.py
@@ -1,8 +1,9 @@
 import argparse
+import os
+import os.path
 import shutil
-from os import makedirs
-from os import path
 
+import click
 import mysql.connector
 from anytree import Node, LevelOrderIter
 
@@ -19,6 +20,7 @@ def parse_args():
     parser.add_argument('--password', dest='password')
     parser.add_argument('--port', dest='port', default='3306')
     parser.add_argument('--db', dest='db')
+    parser.add_argument('--action', choices=['fake', 'hardlink', 'copy'], default='fake')
     parser.add_argument('SOURCE', help='Source folder that contains the "hashed" folder')
     parser.add_argument('TARGET', help='Target folder to copy the files to')
 
@@ -67,21 +69,35 @@ def build_file_nodes(rows):
 
 def build_nodes(rows):
     return (Node(id=id, name=fname, fname=fname, parent_id=parent_id)
-                for id, parent_id, fname in rows)
+            for id, parent_id, fname in rows)
 
 
 def build_tree(folder_rows, file_rows):
     root = Node(name='', fname='', id=0, parent_id=-1)
     node_by_id = {node.id: node for node in build_nodes(folder_rows)}
     node_by_id[0] = root
-    files_itr = build_file_nodes(file_rows)
-    for file in files_itr:
-        folder_node = node_by_id[file.folder_id]
-        file.parent = folder_node
+    with click.progressbar(build_file_nodes(file_rows), label='Connecting files') as itr:
+        for file in itr:
+            folder_node = node_by_id[file.folder_id]
+            file.parent = folder_node
     for node in node_by_id.values():
         if node.parent_id is not None:
             node.parent = node_by_id.get(node.parent_id)
     return root
+
+
+def fake_operation(source, target, _):
+    print(source, '->', target)
+
+
+def copy_operation(source, target, target_folder):
+    os.makedirs(target_folder, exist_ok=True)
+    shutil.copy(source, target)
+
+
+def hardlink_operation(source, target, target_folder):
+    os.makedirs(target_folder, exist_ok=True)
+    os.link(source, target)
 
 
 def main():
@@ -91,16 +107,26 @@ def main():
     paths = []
     for node in LevelOrderIter(root):
         if hasattr(node, 'location'):
-            paths.append((path.join(*(n.fname for n in node.path[:-1])), node.fname, node.location))
+            paths.append((os.path.join(*(n.fname for n in node.path[:-1])), node.fname, node.location))
 
     source_root = args.SOURCE
     target_root = args.TARGET
-    for folder, filename, source in paths:
-        target_folder = path.join(target_root, folder)
-        target_file = path.join(target_folder, filename)
-        source_path = path.join(source_root, source)
-        makedirs(target_folder, exist_ok=True)
-        shutil.copy(source_path, target_file)
+    operation = {
+        'fake': fake_operation,
+        'copy': copy_operation,
+        'hardlink': hardlink_operation
+    }[args.action]
+    label = {
+        'fake': 'Faking file copy',
+        'copy': 'Copying files',
+        'hardlink': 'Hard linking files'
+    }[args.action]
+    with click.progressbar(paths, label=label) as path_itr:
+        for folder, filename, source in path_itr:
+            target_folder = os.path.join(target_root, folder)
+            target_file = os.path.join(target_folder, filename)
+            source_path = os.path.join(source_root, source)
+            operation(source_path, target_file, target_folder)
 
 
 if __name__ == '__main__':
